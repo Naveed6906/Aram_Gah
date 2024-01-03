07d065f764e2400302488e1ab43428298b513076
Naveed Ahmed Shaikh <naveedshaikh2425770@gmail.com>
2024-01-03 12:52:38 +0500

    Initial commit

---
+++ app/src/main/java/com/iba/bt/aramgah/login_screen.kt
@@ -0,0 +1,11 @@
+package com.iba.bt.aramgah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
